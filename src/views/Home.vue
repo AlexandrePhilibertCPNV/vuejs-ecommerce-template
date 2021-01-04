7c6a4eb05395e30bbd6578b0c2c08b478624769c
Alexandre Philibert <=>
2021-01-04 16:45:38 +0100

    Add Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <Jumbotron />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header';
+import Jumbotron from '../components/Jumbotron';
+import Footer from '../components/Footer';
+
+export default {
+  components: {
+    Header,
+    Jumbotron,
+    Footer,
+  },
+};
+</script>
