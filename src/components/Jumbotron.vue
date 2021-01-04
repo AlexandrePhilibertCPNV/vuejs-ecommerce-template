83fe7004e5aa65dcb3d1629746f9294d334d5fee
Alexandre Philibert <=>
2021-01-04 15:10:43 +0100

    Create Jumbotron component

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="" alt="" />
+    <h1>{{title}}</h1>
+    <div class="discount">20%</div>
+    <div><span>Jusqu'au</span><time>31 Janvier 2020</time></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
