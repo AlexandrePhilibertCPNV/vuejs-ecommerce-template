6221dcca3c5772a2ed1af93ed704c1ec1c750645
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-01-26 09:48:29 +0100

    Add QuantityPicker component

--- src/components/QuantityPicker.vue
+++ src/components/QuantityPicker.vue
@@ -1,13 +1,53 @@
 <template>
-  <div></div>
+  <div class="picker">
+    <button @click="decrement">-</button>
+    <input class="quantity" type="number" min="1" :value="quantity" />
+    <button @click="increment">+</button>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      quantity: 1,
+    };
+  },
+  methods: {
+    decrement() {
+      this.quantity = Math.max(this.quantity - 1, 1);
+    },
+    increment() {
+      this.quantity++;
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="sass" scoped>
+.picker
+  display: flex
+  flex-flow: row nowrap
 
+.quantity
+  width: 2em
+  border: none
+  border-top: 1px solid #d7d7d7
+  border-bottom: 1px solid #d7d7d7
+  text-align: center
+  background-color: #F2F2F2
+
+button
+  padding: .25em .65em
+  border: 1px solid #d7d7d7
+  background: none
+  cursor: pointer
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button
+  -webkit-appearance: none
+  margin: 0
+
+input[type=number]
+  -moz-appearance: textfield
 </style>
