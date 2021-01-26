651f06ea0a3483c0f1b8d0e1879808168ff6d2bb
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-01-26 08:58:55 +0100

    Add Rating component

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -1,13 +1,38 @@
 <template>
-  <div></div>
+  <div class="rating">
+    <button><StarSVG /></button>
+    <button><StarSVG /></button>
+    <button><StarSVG /></button>
+    <button><StarHalfSVG /></button>
+    <button><StarOutlineSVG /></button>
+  </div>
 </template>
 
 <script>
-export default {
+import StarSVG from "../assets/star.svg";
+import StarOutlineSVG from "../assets/star-outline.svg";
+import StarHalfSVG from "../assets/star-half.svg";
 
-}
+export default {
+  components: {
+    StarSVG,
+    StarOutlineSVG,
+    StarHalfSVG,
+  },
+};
 </script>
 
-<style>
+<style lang="sass" scoped>
+.rating
+  display: flex
+  flex-flow: row nowrap
 
+button
+  padding: 0
+  border: none
+  background: none
+  cursor: pointer
+
+  & > svg
+    fill: #f39e1d
 </style>
